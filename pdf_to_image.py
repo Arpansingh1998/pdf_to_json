ccd5965d2b6b5b5164e2d2f0ac17f74108f60d6e
Arpansingh1998 <arpan@nirontech.com>
2025-09-17 17:45:17 +0530

    val.json

--- pdf_to_image.py
+++ pdf_to_image.py
@@ -7,13 +7,13 @@ def pdf_to_images(pdf_path, output_folder="pdf_images", dpi=300):
     
     image_paths = []
     for i, img in enumerate(images):
-        img_path = os.path.join(output_folder, f"Picking_Slip_F43987155.png")
+        img_path = os.path.join(output_folder, f"234.png")
         img.save(img_path, "PNG")
         image_paths.append(img_path)
     
     return image_paths
 
 # Example usage
-pdf_file = "ram.pdf"
+pdf_file = "234.pdf"
 image_files = pdf_to_images(pdf_file)
 print(f"Converted {len(image_files)} pages to images.")
